82237cd7b9a346d4465ae4fb262eb180e0b2d70d
kidstonek <96789294+kidstonek@users.noreply.github.com>
2024-09-23 23:25:04 +0300

    1 and 3

---
+++ Ex_08_03.py
@@ -0,0 +1,49 @@
+"""
+Завдання 3
+
+Створіть список товарів в інтернет-магазині.
+Серіалізуйте його за допомогою pickle та збережіть у JSON.
+
+"""
+
+import pickle
+import json
+
+def main():
+    internet_shop = [
+    {
+        "category": "toys",
+        "item": 'soldier',
+        'price': 45
+    },
+    {
+        "category": "flowers",
+        "item": 'lylies',
+        'price': 10
+    },
+    {
+        "category": "electronics",
+        "item": 'iron',
+        "price": 400
+    },
+    {
+        "category": "electronics",
+        "item": "tv",
+        "price": 10000
+    },
+    {
+        "category": "books",
+        "item": 'sons III',
+        'price': 4
+    },
+]
+
+    with open('data.pickle', 'wb') as file:
+        pickle.dump('internet_shop', file)
+
+    with open('data_json.json', 'w') as file:
+        json.dump(internet_shop, file, indent = 2, ensure_ascii = False)
+
+
+if "__main__" == __name__:
+    main()
