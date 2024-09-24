d503224d2a9a4065e50797c2b8b3478622fe74e0
ava <kidstonek@gmail.com>
2024-09-24 22:09:44 +0300

    simple shelve

--- Ex_08_02.py
+++ Ex_08_02.py
@@ -6,32 +6,43 @@
 ознайомитися з модулем shelve (https://docs.python.org/3/library/shelve.html),
 який у даному випадку буде дуже зручним та спростить виконання завдання.
 """
-import  shelve
 
-my_lib = {}
+
+import shelve
+
+
 while True:
     print("choose what to do:\n1) add items:\n2) search by name\n3) show all")
     option = input('choose options: ')
     if option == '1':
-        while True:
-            big_url = input('введіть полний URL: ')
-            if big_url:
-                print('Єнтер для закінчення вводу')
-                name = input('введіть назву: ')
-                my_lib[name] = big_url
-            else:
-                print('end of input!')
-                break
+        with shelve.open('my_shelve.db') as my_lib:
+            while True:
+                print('Return для закінчення вводу')
+                big_url = input('введіть повний URL: ')
+                if big_url:
+                    name = input('введіть короткий URL: ')
+                    my_lib[name] = big_url
+                else:
+                    print('end of input!')
+                    break
     elif option == '2':
         print('what to search?')
         search_name = input("what name? ")
-        if my_lib[search_name]:
-            print(my_lib[search_name])
-        else:
-            print('nothing found')
-            break
+        with shelve.open('my_shelve.db') as my_lib:
+            found_something = False
+            for key in my_lib:
+                if key.lower() == search_name.lower():
+                    print(my_lib[key])
+                    input()
+                    found_something = True
+                    break
+                continue
+            if not found_something:
+                input('nothing found')
     elif option == '3':
-        print(my_lib)
+        with shelve.open('my_shelve.db') as my_lib:
+            for key in my_lib:
+                print(f'short url: {key} normal url {my_lib[key]}')
     else:
         print('end!')
         break
